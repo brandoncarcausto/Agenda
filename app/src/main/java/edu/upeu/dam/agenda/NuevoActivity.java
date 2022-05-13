c96e56301a6edbe523260fe05332702abae49d5f
brandoncarcausto <brandon.cs@upeu.edu.pe>
2022-05-12 19:13:16 -0500

    Initial commit

---
+++ app/src/main/java/edu/upeu/dam/agenda/NuevoActivity.java
@@ -0,0 +1,49 @@
+package edu.upeu.dam.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.upeu.dam.agenda.db.DbContactos;
+
+public class NuevoActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreoElectronico;
+    Button btnGuarda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
+        btnGuarda = findViewById(R.id.btnGuarda);
+
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbContactos dbContactos = new DbContactos(NuevoActivity.this);
+                long id = dbContactos.insertarContactos(txtNombre.getText().toString(),txtTelefono.getText().toString(),
+                txtCorreoElectronico.getText().toString());
+
+                if (id>0){
+                    Toast.makeText(NuevoActivity.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
+                    limpiar();
+                }else{
+                    Toast.makeText(NuevoActivity.this,"ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void limpiar(){
+        txtNombre.setText("");
+        txtTelefono.setText("");
+        txtCorreoElectronico.setText("");
+    }
+}
---
+++ app/src/main/java/edu/upeu/dam/agenda/db/DbContactos.java
@@ -0,0 +1,44 @@
+package edu.upeu.dam.agenda.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbContactos extends DbHelper{
+
+    Context context;
+
+    public DbContactos(@Nullable Context context) {
+
+        super(context);
+        this.context = context;
+
+    }
+
+    public long insertarContactos(String nombre, String telefono, String correo_electronico) {
+
+        long id=0;
+
+        try {
+
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("telefono", telefono);
+        values.put("correo_electronico", correo_electronico);
+
+        id = db.insert(TABLE_CONTACTOS, null, values);
+    } catch(Exception ex){
+            ex.toString();
+        }
+        return id;
+
+
+    }
+
+}
